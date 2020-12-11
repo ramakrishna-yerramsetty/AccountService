fa9ee0cdf2029f636a207b47927ea8c0db361904
rdebock <rdebock@americanfirstfinance.com>
2020-12-11 14:36:31 -0600

    Rein -- Input validation of details endpoint, plus return 404 from details endpoint if account not found.

--- src/main/java/com/americanfirstfinance/account/controller/AccountDetailsController.java
+++ src/main/java/com/americanfirstfinance/account/controller/AccountDetailsController.java
@@ -6,31 +6,44 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
+import javax.validation.Valid;
+import javax.validation.Validator;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Path("/poc/details")
 public class AccountDetailsController {
     private static final Logger LOGGER = LoggerFactory.getLogger(AccountDetailsController.class);
 
+    private final Validator validator;
+
     private final AccountDetailsHandler accountDetailsHandler;
 
     @Inject
-    public AccountDetailsController(AccountDetailsHandler accountDetailsHandler) {
+    public AccountDetailsController(Validator validator, AccountDetailsHandler accountDetailsHandler) {
+        this.validator = validator;
         this.accountDetailsHandler = accountDetailsHandler;
     }
 
     @GET
     @Path("/{customerNumber}/{accountId}")
     @Produces(MediaType.APPLICATION_JSON)
-    public AccountDetailsView getAccountDetails(@PathParam("customerNumber") String customerNumber, @PathParam("accountId") String accountId) {
+    public Response getAccountDetails(@PathParam("customerNumber") @NotNull @Pattern(regexp = "[0-9]{1,6}") @Valid String customerNumber,
+                                      @PathParam("accountId") @NotNull @Pattern(regexp = "[0-9]{1,5}") @Valid String accountId) {
         LOGGER.info("getAccountDetails() -- customerNumber: {}  accountId: {}", customerNumber, accountId);
         AccountDetailsView view = accountDetailsHandler.getAccountDetails(customerNumber, accountId);
 
-        return view;
+        if (view == null) {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        } else {
+            return Response.ok(view).build();
+        }
 
     }
 }
